96ce2b94f466eaa3049ed501e9790434b90b4d7d
Martin Qian <mackuntu@gmail.com>
2016-05-01 09:24:26 -0700

    automate all donations

--- royale.sikuli/royale.py
+++ royale.sikuli/royale.py
@@ -1,3 +1,4 @@
+import random as r
 open_sign = Pattern("open.png").similar(0.40)
 unlock = "unlock.png"
 start_unlock = "1462034142273.png"
@@ -5,13 +6,13 @@ chest_open = Pattern("chest_open.png").similar(0.82)
 clash_icon = "clash_icon.png"
 clash_notification = "clash_notification.png"
 home_screen = "home_screen.png"
-battle = "battle.png"
+battle = Pattern("battle.png").similar(0.81)
 msg_window = "msg_window.png"
-card_request = "card_request.png"
+card_request = Pattern("card_request.png").similar(0.79)
 minions = "minions.png"
 goblins = "goblins.png"
-
-App.focus("player")
+donate = Pattern("donate.png").similar(0.85)
+more_donations = Pattern("more_donations.png").exact()
 
 click_location = None
 
@@ -19,6 +20,7 @@ def clickChest():
   while not exists(battle):
     try:
       click_location.click()
+      sleep(2)
     except:
       print "cannot click"
       break
@@ -32,9 +34,8 @@ def openChestIfAvailable():
       chest.click()
       sleep(1)
       clickChest()
-  except e:
+  except:
     print "no open chests found"
-    print e
 
 def unlockChestIfAvailable():
   if exists(unlock):
@@ -60,19 +61,44 @@ def requestCardsIfAvailable():
     print "found minions to request"
     click(minions)
 
+def donateAsMuchAsPossible():
+  try:
+    all_donations = findAll(donate)
+    print "found donations"
+    for donation in all_donations:
+      print "donating..."
+      donation.highlight(1)
+      donation.click()
+      donation.click()
+  except:
+    print "did not find any more donations"
+  try:
+    if exists(more_donations):
+      print "more donations may be possible"
+      click(more_donations)
+      sleep(2)
+      donateAsMuchAsPossible()
+  except:
+    print "no more donations available"
 
-def openClashIfNotification():
-  while not exists(clash_notification):
-    sleep(60 * 60 * 3)
-  click(clash_icon)
-  while not exists(battle):
-    sleep(5)
+def startWorkflow():
+  App.focus("player")
+  global click_location
   click_location = find(battle)
   openChestIfAvailable()
   unlockChestIfAvailable()
   requestCardsIfAvailable()
+  donateAsMuchAsPossible()
+
+def openClashIfNotification():
+  while not exists(clash_notification):
+    sleep(r.randint(60,3600))
+    App.focus("player")
+  click(clash_icon)
+  while not exists(battle):
+    sleep(5)
+  startWorkflow()
   click(home_screen)
   openClashIfNotification()
 
-#openClashIfNotification()
-#requestCardsIfAvailable()
+openClashIfNotification()
